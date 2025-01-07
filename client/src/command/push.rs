9cbf4d53575838299e7709bb98a5b6e64d237a3c
cy <hi@cything.io>
2025-01-07 15:48:03 -0500

    set default parallel uploads to 32

--- client/src/command/push.rs
+++ client/src/command/push.rs
@@ -39,7 +39,7 @@ pub struct Push {
     ignore_upstream_cache_filter: bool,
 
     /// The maximum number of parallel upload processes.
-    #[clap(short = 'j', long, default_value = "5")]
+    #[clap(short = 'j', long, default_value = "32")]
     jobs: usize,
 
     /// Always send the upload info as part of the payload.
