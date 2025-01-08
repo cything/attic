a6d41b662b8bc42eceebacdd53d55d6c03398540
cy <hi@cything.io>
2025-01-07 22:01:02 -0500

    try not to use legacy stuff

--- flake/nix-versions.nix
+++ flake/nix-versions.nix
@@ -36,8 +36,6 @@ in
       attic.nix-versions = {
         versions = {
           default = pkgs.nix;
-          "2.20" = pkgs.nixVersions.nix_2_20;
-          "2.24" = pkgs.nixVersions.nix_2_24;
         };
 
         manifestFile = let
